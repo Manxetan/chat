8c54dbb908d8a798d0b730420a26b407ccdacf6a
Manxetan <manxetan92@gmail.com>
2017-12-27 20:20:50 +0500

    1

---
+++ chat_c/chat_c/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroMQ;
+
+namespace chat_c
+{
+    static partial class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            if (args == null || args.Length < 1)
+            {
+                args = new string[] { "tcp://10.168.78.254:8080" };
+            }
+
+            string endpoint = args[0];
+
+            string UserName;
+            Console.Write("Enter your name: ");
+            UserName = Console.ReadLine();
+
+            // Create
+            using (var context = new ZContext())
+            using (var requester = new ZSocket(context, ZSocketType.REQ))
+            {
+                // Connect
+                requester.Connect(endpoint);
+
+
+                while (true)
+                {
+                    string requestText;
+                    Console.Write("Enter your message: ");
+                    requestText = UserName + ": " + Console.ReadLine();
+                    // Send
+                    requester.Send(new ZFrame(requestText));
+
+                    // Receive
+                    using (ZFrame reply = requester.ReceiveFrame())
+                    {
+
+                        Console.WriteLine(reply.ReadString());
+                        //Thread.Sleep(10000);
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ chat_s/chat_s/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeroMQ;
+using System.Threading;
+
+namespace chat_s
+{
+    static partial class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            args = new string[] { "" };
+
+            // Create
+            using (var context = new ZContext())
+            using (var responder = new ZSocket(context, ZSocketType.REP))
+            {
+                // Bind
+                responder.Bind("tcp://*:5570");
+
+                while (true)
+                {
+                    // Receive
+                    using (ZFrame request = responder.ReceiveFrame())
+                    {
+                        string k;
+                        int i = 0;
+                        k = request.ReadString();
+                        Console.WriteLine(k);
+
+                        // Do some work
+                        Thread.Sleep(1);
+                        // Send 
+                        args = new string[] { k };
+                        string name = args[i];
+
+                        responder.Send(new ZFrame(args[i]));
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+}
